25a8b717aad98a9f5c51dfd44ed6bcdec1c8ceab
jimipulsar <jimipulsar@gmail.com>
2019-02-28 17:08:43 +0100

    Modifica di stile

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,27 +5,29 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h2>Modifica il post {{$posts->title}}</h2>
-				<form class="form-contatto" action="{{ route('posts.update', $posts->id)}}" method="POST">
+				<h2 class="terzo-titolo">Modifica il post {{$posts->title}}</h2>
+				<form action="{{ route('posts.update', $posts->id)}}" method="POST">
 					@method('PUT')
 					@csrf
-			<div class="form-group">
-			  <label for="title">Titolo</label>
-			  <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo" value="{{$posts->title}}">
-			</div>
-			<div class="form-group">
-				<label for="author">Autore</label>
-				<input type="text" name="author" class="form-control" placeholder="Inserisci l'autore" value="{{$posts->author}}">
-			</div>
-			<div class="form-group">
-				<label for="category_id">Categoria</label>
-				<input type="text" name="category_id" class="form-control" placeholder="Inserisci la categoria" value="{{$posts->category_id}}">
-			</div>
-			<div class="form-group">
-				<label for="content">Descrizione</label>
-			  <input type="text" name="content" class="form-control" placeholder="Inserisci la descrizione" value="{{$posts->content}}">
-			<div class="form-group">
-				<input type="submit" value="Salva nuovo articolo" class="form-control" id="save">
+			<div class="form-edit">
+				<div class="form-group">
+				  <label for="title">Titolo</label>
+				  <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo" value="{{$posts->title}}">
+				</div>
+				<div class="form-group">
+					<label for="author">Autore</label>
+					<input type="text" name="author" class="form-control" placeholder="Inserisci l'autore" value="{{$posts->author}}">
+				</div>
+				<div class="form-group">
+					<label for="category_id">Categoria</label>
+					<input type="text" name="category_id" class="form-control" placeholder="Inserisci la categoria" value="{{$posts->category_id}}">
+				</div>
+				<div class="form-group">
+					<label for="content">Descrizione</label>
+				  <input type="text" name="content" class="form-control" placeholder="Inserisci la descrizione" value="{{$posts->content}}">
+				<div class="form-group">
+					<input type="submit" value="Salva nuovo articolo" class="form-control" id="save">
+				</div>
 			</div>
 		</div>
 	</div>
